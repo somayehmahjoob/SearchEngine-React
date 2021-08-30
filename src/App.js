9b47e7413a5a11e7cf2fbdd81e0a6d983a9e2521
Somayeh mahjoob <somayehmahjob@gmail.com>
2021-08-30 21:42:14 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Weather Search Engine</h1>
-        <SearchEngine />      
+    <div>
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+          <SearchEngine />      
+      </div>
+      <div className="footer">
+        <a href="https://github.com/somayehmahjoob/SearchEngine-React">Open-source code</a>, by Somayeh.Mahjoob
+      </div>
     </div>
   );
 }
